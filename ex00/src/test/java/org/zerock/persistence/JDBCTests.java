ddcbb39afc27387517e4a97512c9d8164eb6cbc8
ej <ejoo1109@nate.com>
2021-03-26 13:28:17 +0900

    0325 스프링 테스트 연습

---
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,32 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class JDBCTests {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try(Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:orcl",
+				"javaDB",
+				"12345")) {
+			log.info(""+con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
