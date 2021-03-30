ef99e35a7524ea9ebfeadb0cd999e15b95651af7
ej <ejoo1109@nate.com>
2021-03-30 21:09:14 +0900

    0330 댓글 crud 작업중

---
+++ ex03/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,27 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+public interface BoardMapper {
+
+	public List<BoardVO> getList();
+
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+	//Criteria 타입을 파라메터로 사용하는 메서드
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	//전체 게시물 수
+	public int getTotalCount(Criteria cri);
+	
+}
---
+++ ex03/src/test/java/org/zerock/controlle/ReplyMapperTests.java
@@ -0,0 +1,81 @@
+package org.zerock.controlle;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.ReplyMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Slf4j
+public class ReplyMapperTests {
+
+	@Autowired
+	private ReplyMapper mapper;
+	
+	//db에 실존하는 게시글 번호
+	private Integer[] bnoArr = {776,772,769,767,766};
+	@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			ReplyVO vo = new ReplyVO();
+			
+			//게시물의 번호
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글테스트" +i );
+			vo.setReplyer("replyer" + i);
+			
+			mapper.insert(vo);
+		});
+	}
+	@Test
+	public void testMapper() {
+		log.info(""+mapper);
+	}
+	
+	@Test
+	public void testRead() {
+		
+		int targetRno = 5;
+		ReplyVO vo = mapper.read(targetRno);
+		
+		log.info(""+vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		int targetRno = 1;
+		mapper.delete(targetRno);
+	}
+	
+	@Test
+	public void testUpdate() {
+		int targetRno = 10;
+		ReplyVO vo = mapper.read(targetRno);
+		vo.setReply("Update Reply");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: " + count);
+	}
+	
+	@Test
+	public void testList() {
+		Criteria cri = new Criteria();
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		
+		replies.forEach(reply -> log.info(""+reply));
+	}
+	
+}
